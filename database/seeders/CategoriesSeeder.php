93cb45c0eab7757e78e84d10ec7e14e11f445b3c
Agustinus <57866141+lgarin211@users.noreply.github.com>
2024-08-27 04:52:26 +0000

    modified:   .env

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategoriesSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class CategoriesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // insert categories Regional,Law and Society,Tecknology,Entertainment
+        $categories = ['Regional', 'Law and Society', 'Tecknology', 'Entertainment'];
+        foreach ($categories as $category) {
+            DB::table('categories')->insert([
+                'name' => $category,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // call UserSeeder
+        $this->call(UserSeeder::class);
+        // call CategoriesSeeder
+        $this->call(CategoriesSeeder::class);
+        // call ArticleSeeder
+        $this->call(ArticleSeeder::class);
     }
 }
